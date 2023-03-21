077df35fc0c2247254cd406f19f0a43abfbf61f4
wiwabzu <wiwabzu@outlook.com>
2023-03-21 20:27:33 +0800

    test

---
+++ Objectliterals.js
@@ -0,0 +1,11 @@
+//object literals
+const tao = {
+    firstNAme: "Pedro",
+    lastNAme: "Penduko",
+    age: 35,
+    favorites : ["apple", "blue","english", "anime"],
+    familyMembers: {
+        
+    }
+
+}
