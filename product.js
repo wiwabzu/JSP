979a0158ed69a55d215475c43a5d0c44b2311fbd
wiwabzu <wiwabzu@outlook.com>
2023-03-21 22:18:39 +0800

    sample searbar success by not using chat gpt

--- product.js
+++ product.js
@@ -1,10 +1,10 @@
-function search_products() {
+function search_product() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
     let x = document.getElementsByClassName('products');
 
-    for (i = 0; i < x.length; i++){
-        if (!x[i].innerHTML.toLowerCase().includes(input)){
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
         }
         else {
@@ -12,3 +12,18 @@ function search_products() {
         }
     }
 }
+
+// function search_product() {
+//     let input = document.getElementById('searchbar').value.toLowerCase();
+//     let products = document.getElementsByClassName('products');
+  
+//     for (let i = 0; i < products.length; i++) {
+//       let productName = products[i].querySelector('.product-name').textContent.toLowerCase();
+//       if (!productName.includes(input)) {
+//         products[i].style.display = "none";
+//       } else {
+//         products[i].style.display = "list-item";
+//       }
+//     }
+//   }
+  
